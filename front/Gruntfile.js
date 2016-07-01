fbc553065dfd95459849ec89e0df2425e8b37968
Leandro Medeiros <leandrotinfo19@gmail.com>
2016-06-30 21:33:49 -0300

    Arrumando diretorios
    testando novos dir

--- front/Gruntfile.js
+++ front/Gruntfile.js
@@ -1,66 +1,68 @@
-module.exports = function(grunt) {
-
-   grunt.initConfig({
-      clean: {
-         temp: ['../confs/front/css/libs.css','../confs/front/js/libs.js'],
-         all: []
-      },
-      stylus: {
-         options: {
-            use: [
-               require('jeet'),
-               require('rupture'),
-               function() { return require('autoprefixer-stylus')({browsers: ["> 0%"]}); }
-            ],
-            paths: [
-               './node_modules/rupture',
-               './node_modules/jeet/stylus',
-            ],
-         },
-         files: {
-            src : 'files/stylesheet/*.styl',
-            dest : '../confs/front/css/main.min.css',
-         }
-      },
-      uglify: {
-         my_target: {
-            files: {
-               '../confs/front/js/libs.min.js': ['../confs/front/js/libs.js'],
-            }
-         }
-      },
-      cssmin: {
-         options: {
-            shorthandCompacting: false,
-            roundingPrecision: -1
-         },
-         target: {
-            files: {
-               '../confs/front/css/libs.min.css' : ['../confs/front/css/libs.css']
-            }
-         }
-      },
-      watch: {
-         css: {
-            files: 'files/stylesheet/**/*.styl',
-            tasks: ['stylus'],
+module.exports = function (grunt) {
+    grunt.initConfig({
+        uglify: {
+            dynamic_mappings: {
+                files: [{ 
+                    expand: true,    // allow dynamic building
+                    flatten: true,   // remove all unnecessary nesting
+                    cwd: 'files/scripts/',
+                    src: '**/*.js',  // source files mask
+                    dest: '../confs/front/js/',    // destination folder
+                    ext: '.min.js'   // replace .js to .min.js
+                }]
+            },
+        },
+        less: {
             options: {
+               paths: ["css"],
+               cleancss: true,
+               compress: true,
+            },
+            files: {
+               expand: true,
+               cwd: 'files/less/themes/',
+               src: ['*.less'],
+               dest: '../confs/front/css/',
+               ext: '.min.css'
+            }
+        },
+        sprite: {
+            all: {
+               src: 'files/sprites/*.png',
+               dest: '../confs/front/sprites/spritesheet.png',
+               destCss: '../confs/front/css/sprites.css'
+            }
+        },
+        watch: {
+            options: {
+               spawn: false,
                livereload: true,
             },
-         },
-         js: {
-            files: ['files/scripts/*.js'],
-            files: ['files/scripts/**/*.js'],
-            tasks: ['uglify']
-         },
-      },
-   });
+            js: {
+               files: ['files/scripts/*.js'],
+               tasks: ['uglify']
+            },
+            css: {
+               files: ['files/less/**/*.less'],
+               tasks: ['less']
+            },
+        }
 
-   grunt.loadNpmTasks('grunt-contrib-stylus');
-   grunt.loadNpmTasks('grunt-contrib-uglify');
-   grunt.loadNpmTasks('grunt-contrib-clean');
-   grunt.loadNpmTasks('grunt-contrib-cssmin');
-   grunt.loadNpmTasks('grunt-contrib-watch');
+    });
 
-   grunt.registerTask('default', ['uglify', 'stylus', 'clean', 'watch']);
+    //Load Tasks
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    //Tasks
+    grunt.registerTask('default', ['uglify', 'less', 'watch']);
+
+    // Build
+    grunt.loadNpmTasks('grunt-spritesmith'); // Fazendo sprites 
+   
+    
+    //Watch task
+    //grunt.registerTask('w', ['watch']);
+
 };
